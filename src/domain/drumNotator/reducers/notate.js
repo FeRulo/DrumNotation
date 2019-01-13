9e20869a6dab355f87deb733186f1eebee924e37
Fercho <josepaez@seven4n.com>
2019-01-13 11:46:14 -0500

    only two drums in each element notation

--- src/domain/drumNotator/reducers/notate.js
+++ src/domain/drumNotator/reducers/notate.js
@@ -1,17 +1,16 @@
 import {contains,
-  bits,
   kick1KeyCombination,
   snare1KeyCombination,
   snare2KeyCombination,
   kick0KeyCombination } from '../vos/keyConstantValues'
 
 
-const getBinaries =(keyMap=[], combinations, bits)=>{
+const getBinariesByKeyCombinations =(keyMap=[], combinations)=>{
   if(combinations.length>0 && keyMap.length>0){
-    return ( combinations[0].every(k=>contains(keyMap,k)) && 
-            combinations[0].length===keyMap.length? 
-            bits[0]: 0 ) 
-    + getBinaries(keyMap,combinations.slice(1),bits.slice(1))
+    return ( combinations[0].keys.every(k=>contains(keyMap,k)) && 
+            combinations[0].keys.length===keyMap.length? 
+            combinations[0].binaryValue: 0 ) 
+    + getBinariesByKeyCombinations(keyMap,combinations.slice(1))
   } 
   else return 0
 }
@@ -25,10 +24,8 @@ const notate = (state = [{
   let keys = [action.key,...action.keyMap.map(k=>k.key)].filter((k,i,self)=>self.indexOf(k)===i)
   return [
       {...state[0],
-        snare1: state[0].snare1 ^ getBinaries(keys,snare1KeyCombination,bits),
-        snare2: state[0].snare2 ^ getBinaries(keys,snare2KeyCombination,bits),
-        kick0: state[0].kick0 ^ getBinaries(keys,kick0KeyCombination,bits),
-        kick1: state[0].kick1 ^ getBinaries(keys,kick1KeyCombination,bits),
+        snare1: state[0].snare1 ^ getBinariesByKeyCombinations(keys,snare1KeyCombination),
+        kick0: state[0].kick0 ^ getBinariesByKeyCombinations(keys,kick0KeyCombination),
       }, 
       ...state.slice(1)]
 }
