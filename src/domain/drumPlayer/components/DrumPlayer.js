cee269fbf9b568fe87263e529a04aafc1920c502
Fercho <josepaez@seven4n.com>
2019-01-24 23:10:07 -0500

    playing all letters

--- src/domain/drumPlayer/components/DrumPlayer.js
+++ src/domain/drumPlayer/components/DrumPlayer.js
@@ -1,33 +1,48 @@
 import React,{ Component } from 'react';
 import audioKick from '../../../assets/Kick.wav'
 import audioSnare from '../../../assets/Snare.wav'
+import {pause, stop, follow} from '../dispatchers/playerDispatcher'
 
 const fractions=(b,a=0)=>{ return a>b? []:[a/b,...fractions(b,a+1)]} 
 
 const intervals = [...fractions(6),...fractions(8)]
+    .flatMap(d=>[d,d+1])
     .sort((a,b)=>{return a-b})
-    .filter((x,i,array)=>{return i===array.indexOf(x)})
-    
+    .filter((x,i,array)=>{return i===array.indexOf(x)})//no repeated elements
+
+console.log(intervals)
+
 const interval = (i)=> intervals[Math.log2(i)]
 
-const getTimeBetween = (i0,i1,bpm)=> {
-    return (60000/bpm)*(interval(i1)-interval(i0))
+const getTimeBetween = (player,next,bpm)=> {  
+    console.log(`${interval(next.indexHit)}-${interval(player.index) }+${next.indexLetter - player.indexLetter}`)  
+    return (60000/bpm)*(
+        interval(next.indexHit)-
+        (next.indexHit===1? 0:interval(player.index)) +
+        next.indexLetter - player.indexLetter)
 }
-
-const followWith =(
-        index,
+function getKickLetter(indexLetter, indexHit,notation){
+    return (indexHit >= (1<<12) && indexLetter < notation.length - 1)? 
+        (indexLetter + 1):
+         indexLetter
+}
+function getKickHit(indexHit){
+    return (indexHit >= (1<<12))? indexHit>>12: indexHit<<12
+}
+const getNextIndexes =(
+        indexHit,
+        indexLetter,
         notation)=>{
-    if(bitsAreMatching(notation[0].snare, index) ||
-        bitsAreMatching(notation[0].kick<<12, index)){
-        return index
+    let kickLetter = getKickLetter(indexLetter,indexHit,notation)
+    let kickHit = getKickHit(indexHit)
+    if(indexLetter >= notation.length) return {indexHit:indexHit,indexLetter:indexLetter}
+    else if(bitsAreMatching(notation[indexLetter].snare, indexHit) ||
+        bitsAreMatching(notation[kickLetter].kick, kickHit)){
+        return {indexHit:indexHit,indexLetter:indexLetter}
     }
     else{
-        if(Math.log2(index) > intervals.length - 1){
-            return 1<<(intervals.length - 1)
-        }else{
-            return followWith(index<<1,
-                notation)
-        }
+        if(indexHit >= (1<<24)) return getNextIndexes(1, indexLetter + 1, notation)
+        else return getNextIndexes(indexHit<<1, indexLetter, notation)
     }
 }
 
@@ -36,46 +51,41 @@ function hit(url){
 }
 
 function bitsAreMatching(drum, index){
-    return drum & index
+    let res = drum & index
+    return res
+}
+function playKick(notation,indexLetter, indexHit){
+    let kickLetter = getKickLetter(indexLetter,indexHit,notation)
+    let kickHit = getKickHit(indexHit)
+    if(bitsAreMatching(notation[kickLetter].kick,kickHit)) {
+        return hit(audioKick)
+    }
+}
+
+function playSnare(notation,indexLetter, indexHit){
+    if(bitsAreMatching(notation[indexLetter].snare,indexHit)) hit(audioSnare)
 }
 
 function play(player,store,notation){
-    if(bitsAreMatching(notation[0].snare,player.index)) hit(audioSnare)
-    if(bitsAreMatching(notation[0].kick,player.index)) hit(audioKick)
-    let nextIndex = followWith(player.index<<1,notation)
-    if(nextIndex===player.index){
-        stop(store)
-    }
+    if(player.indexLetter >= notation.length) stop(store)    
     else{
-        let intervalTime = getTimeBetween(player.index, nextIndex, player.bpm)
+        console.log(`***hit: ${player.index.toString(2)} letter: ${player.indexLetter} 
+            notation: ${notation[player.indexLetter].snare}`)
+        playKick(notation,player.indexLetter,player.index)
+        playSnare(notation,player.indexLetter,player.index)
+        let nextIndexes = getNextIndexes(player.index<<1, player.indexLetter, notation)
+        let intervalTime = getTimeBetween(player, nextIndexes, player.bpm)
         console.log('time waiting '+ intervalTime*player.bpm/60000)
         pause(store)
         return setTimeout(()=>follow(
-        store,nextIndex), 
+            store,nextIndexes), 
         intervalTime)
     }
     
 }
 
 function start(player,store, notation){
-    return player.played && player.continued? play(player,store,notation):0
-}
-function stop(store){
-    store.dispatch({
-        type:'SET_PLAYED'
-    })
-}
-function follow(store,nextIndex){
-    store.dispatch({
-        type:'CONTINUE',
-        index: nextIndex
-    })
-}
-
-function pause(store){
-    store.dispatch({
-        type:'PAUSE'
-    })
+    return player.played && player.continued? play(player,store,notation.slice().reverse()):0
 }
 
 class DrumPlayer extends Component{
--- src/domain/drumPlayer/reducers/player.js
+++ src/domain/drumPlayer/reducers/player.js
@@ -3,6 +3,7 @@ const player = (state={
         played: false,
         continued:false,
         index: 1,
+        indexLetter:0,
         bpm:60
     }, action={type:'NONE'}) => {
     switch (action.type) {
@@ -10,15 +11,18 @@ const player = (state={
             return {played: !state.played, 
                     continued: true,
                     index: 1,
+                    indexLetter:0,
                     bpm:state.bpm}
         case 'PAUSE':
             return {continued: false, 
                     index: state.index,
+                    indexLetter:0,
                     played: state.played,
                     bpm:state.bpm}
         case 'CONTINUE':
             return {continued: true, 
                     index: state.played? action.index:1,
+                    indexLetter: state.played? action.indexLetter:0,
                     played: state.played,
                     bpm:state.bpm}
         case 'INCREASE_BPM':
@@ -29,6 +33,7 @@ const player = (state={
         case 'DECREASE_BPM':
             return {continued: state.continued, 
                     index: state.index,
+                    indexLetter:0,
                     played: state.played,
                     bpm:state.bpm - 1}
         default: return state
